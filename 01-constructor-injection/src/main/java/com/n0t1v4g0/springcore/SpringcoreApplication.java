471eaafb7be4552f9b82d0ef07c6dc17f6c18fd7
Lucas Barbosa <lukeb.trybe@gmail.com>
2023-08-17 17:05:46 -0300

    initial commit

---
+++ 01-constructor-injection/src/main/java/com/n0t1v4g0/springcore/SpringcoreApplication.java
@@ -0,0 +1,13 @@
+package com.n0t1v4g0.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreApplication.class, args);
+	}
+
+}
---
+++ 01-constructor-injection/src/test/java/com/n0t1v4g0/springcore/SpringcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n0t1v4g0.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
